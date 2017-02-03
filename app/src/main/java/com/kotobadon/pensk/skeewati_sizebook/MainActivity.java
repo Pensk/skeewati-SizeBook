500247ea4f2cc3fec1db159056f58446893bd3ed
Pensk <pensk@hotmail.ca>
2017-02-02 19:13:53 -0700

    serializer methods

--- app/src/main/java/com/kotobadon/pensk/skeewati_sizebook/MainActivity.java
+++ app/src/main/java/com/kotobadon/pensk/skeewati_sizebook/MainActivity.java
@@ -3,11 +3,19 @@ package com.kotobadon.pensk.skeewati_sizebook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Serializer serializer;
+    private ArrayList<Person> Persons;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serializer = new Serializer();
     }
 }
--- app/src/main/java/com/kotobadon/pensk/skeewati_sizebook/Serializer.java
+++ app/src/main/java/com/kotobadon/pensk/skeewati_sizebook/Serializer.java
@@ -2,14 +2,26 @@ package com.kotobadon.pensk.skeewati_sizebook;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 /**
  * Created by pensk on 2017/02/02.
  */
 
 public class Serializer {
-    Gson gson;
+    private Gson gson;
+    private String filename;
+    private static final String FILENAME = "file.sav";
 
     public Serializer(){
         gson = new Gson();
+    }
+
+    public ArrayList<Person> loadFromFile(){
+        return new ArrayList<Person>();
+    }
+
+    public void saveToFile(ArrayList<Person> Persons){
+
     }
 }

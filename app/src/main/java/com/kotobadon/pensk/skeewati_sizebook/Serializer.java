4cbedbb52af743d95350322fc34c3bada2c3f732
Pensk <pensk@hotmail.ca>
2017-02-02 18:41:44 -0700

    Serializer class

---
+++ app/src/main/java/com/kotobadon/pensk/skeewati_sizebook/Serializer.java
@@ -0,0 +1,15 @@
+package com.kotobadon.pensk.skeewati_sizebook;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by pensk on 2017/02/02.
+ */
+
+public class Serializer {
+    Gson gson;
+
+    public Serializer(){
+        gson = new Gson();
+    }
+}

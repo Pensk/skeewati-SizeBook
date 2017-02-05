0dabc0cabcaac8959ad9b059686031b87bec473a
Pensk <pensk@hotmail.ca>
2017-02-05 14:48:02 -0700

    new person button and list clicks

--- app/src/main/java/com/kotobadon/pensk/skeewati_sizebook/MainActivity.java
+++ app/src/main/java/com/kotobadon/pensk/skeewati_sizebook/MainActivity.java
@@ -1,9 +1,13 @@
 package com.kotobadon.pensk.skeewati_sizebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Serializer serializer;
     private ArrayList<Person> persons;
     private ListView personList;
+    private TextView personCount;
 
 
     @Override
@@ -19,15 +24,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        personList = (ListView) findViewById(R.id.personList);
+        personCount = (TextView) findViewById(R.id.personCountText);
+
         persons = new ArrayList<Person>();
         serializer = new Serializer();
-        personList = (ListView) findViewById(R.id.personList);
+
 
         Person person1 = new Person("Boi");
         person1.setWaist(5f);
         person1.setBust(2f);
         persons.add(person1);
         persons.add(new Person("Boi 2"));
+
+        personCount.setText(String.valueOf(persons.size()));
+
+        personList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Person p = persons.get(position);
+                Intent intent = new Intent();
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     @Override
